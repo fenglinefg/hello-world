c677363037b1ac6dedb48f6dbcdbfeb01c0302d6
demo <available3099@gmail.com>
2020-06-25 13:04:11 +0800

    weather_pro.js

--- demo2099/task/weather_pro.js
+++ demo2099/task/weather_pro.js
@@ -46,7 +46,7 @@ $[daily($[month]月$[day]日  $[temperatureLow]~$[temperatureHigh]°C  $[weather
     }
 }
 
-const provider = {
+const    provider = {
     heweather_now: {
         api: `https://free-api.heweather.net/s6/weather/now?location=${config.lat_lon.replace(/\s/g, "").replace("，", ",")}&key=${config.huweather_apiKey}`,
         progress: 0, //处理进度:0需处理1已处理2无需处理9报错
@@ -100,18 +100,31 @@ const provider = {
         support: ['$[aqiIcon]', '$[aqi]', '$[aqiDesc]', '$[aqiWarning]']
     }
 }
+
+
+ 
 function location(){
  $task.fetch({
         url: "http://ip-api.com/json"
     }).then(response => {
         try {
             let darkObj = JSON.parse(response.body);
-            record(`地理位置数据-A1-${response.body}`);
+
             if (darkObj.error) {
-                $notify("获取位置", "出错啦", darkObj.error);
+                $notify("获取位置", "出错啦", response+darkObj.error);
             }
-			config.lat_lon = darkObj.lat+darkObj.lon;        
-        } catch (e) {
+			config.lat_lon = darkObj.lat+","+darkObj.lon;
+    provider.heweather_now.api=`https://free-api.heweather.net/s6/weather/now?location=${config.lat_lon.replace(/\s/g, "").replace("，", ",")}&key=${config.huweather_apiKey}`;
+		
+		 provider.heweather_daily.api=`https://free-api.heweather.net/s6/weather/forecast?location=${config.lat_lon.replace(/\s/g, "").replace("，", ",")}&key=${config.huweather_apiKey}`;
+		
+		provider.heweather_air.api=`https://free-api.heweather.net/s6/air/now?location=auto_ip&key=${config.huweather_apiKey}`;
+		
+		provider.heweather_lifestyle.api=`https://free-api.heweather.net/s6/weather/lifestyle?location=${config.lat_lon.replace(/\s/g, "").replace("，", ",")}&key=${config.huweather_apiKey}`;
+		
+		 provider.darksky.api=`https://api.darksky.net/forecast/${config.darksky_api}/${config.lat_lon.replace(/\s/g, "").replace("，", ",")}?lang=${config.lang}&units=si`;
+		provider.aqicn.api=`https://api.waqi.info/feed/geo:${config.lat_lon.replace(/\s/g, "").replace("，", ",").replace(/,/, ";")}/?token=${config.aqicn_api}`;
+} catch (e) {
             console.log(`地理位置获取报错${JSON.stringify(e)}`)
         }
     }, reason => {
