7974218f830218ffed6d89f43961b84897112ce5
demo <available3099@gmail.com>
2020-09-22 02:08:05 -0700

    Update

---
+++ demo2099/js/youtubeplus.js
@@ -0,0 +1,36 @@
+/*
+author:demo
+群组https://t.me/demo2099
+https://api.cercube.com/v2/user
+ */
+ var body = $response.body;
+let url=$request.url;
+if(url.endsWith("access_token")||url.endsWith("login")) {
+$done({
+    body
+});
+}else{
+
+
+body={
+  "id" : 386641,
+  "updatedAt" : "2020-04-08T07:55:03.413Z",
+  "lastName" : "demo",
+  "firstName" : "mt",
+  "blocked" : false,
+  "roleId" : 1,
+  "didMigrate" : true,
+  "countryCode" : "CN",
+  "customerId" : "cus_H3g6yrkdYeL0Lh",
+  "username" : "demo2099",
+  "subscriptions" : [
+
+  ],
+  "email" : "xxxx@gmail.com"
+};
+
+body = JSON.stringify(obj);
+$done({
+    body
+});
+}
