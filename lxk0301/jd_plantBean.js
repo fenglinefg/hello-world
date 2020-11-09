eb87523ca8f160a0e838c41255e508d76b6c260f
demo2099 <available3099@gmail.com>
2020-11-09 11:02:54 +0800

    Update jd_plantBean.js

--- lxk0301/jd_plantBean.js
+++ lxk0301/jd_plantBean.js
@@ -32,7 +32,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //助力好友分享码(最多3个,否则后面的助力失败)
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
-let shareCodes =[];let shareCodesxxxx = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
+let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
                    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
   'fnfkmp5hx2byrqss7h5jr5j2wtnlfimruj4z7ii@4oupleiwuds2ar2uqnvnqehopyndwvjwa2qweri@e7lhibzb3zek27amgsvywffxx7hxgtzstrk2lba@e7lhibzb3zek32e72n4xesxmgc2m76eju62zk3y',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
