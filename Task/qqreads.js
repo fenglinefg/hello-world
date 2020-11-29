b4a36209aca342de3201e15b14d75aa80e0e749c
ziye12 <59244461+ziye12@users.noreply.github.com>
2020-11-29 19:41:57 +0800

    Update qqreads.js

--- Task/qqreads.js
+++ Task/qqreads.js
@@ -191,6 +191,7 @@ function all(){
             setTimeout(function() {
 
      if (i==0)
+console.log(`-------------------------\n\n开始【${jsname+num}】`) 
 qqreadinfo();//用户名
 
 else if (i==1)
@@ -242,7 +243,6 @@ qqreadpick();//领周时长奖励
 else if (i == 16) showmsg();
           else if (i == 17 && num < qqreadhdArr.length - 1) {
             num += 1;
-		  console.log(`-------------------------\n\n开始【${jsname+num}】`)
             all();
           } else if (i == 17 && num == qqreadhdArr.length - 1) {
             $.done();
