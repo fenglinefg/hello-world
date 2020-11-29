40df977d9188216f11ccbc28654f600c1da6f6ab
ziye12 <59244461+ziye12@users.noreply.github.com>
2020-11-30 03:03:48 +0800

    Update qqreads.js

--- Task/qqreads.js
+++ Task/qqreads.js
@@ -315,7 +315,7 @@ return new Promise((resolve, reject) => {
      info =JSON.parse(data)
 
 tz+=
-'========== 【'+info.data.user.nickName+'】 ==========\n'
+'\n========== 【'+info.data.user.nickName+'】 ==========\n'
 
 resolve()
     })
