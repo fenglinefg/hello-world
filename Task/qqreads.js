334fc79169ef367afcdc5991c69a6e7729f06c76
ziye12 <59244461+ziye12@users.noreply.github.com>
2020-11-30 22:10:26 +0800

    Update qqreads.js

--- Task/qqreads.js
+++ Task/qqreads.js
@@ -631,7 +631,7 @@ tz+='【周时长奖励'+(i+1)+'】:领取'+Packageid[i]+'阅豆\n'
 function showmsg() {      
 tz += `\n\n========= 脚本执行时间(TM)：${new Date(new Date().getTime() + 0 * 60 * 60 * 1000).toLocaleString('zh', {hour12: false})} \n\n`;
 	
-if ($.isNode()&&$.time('HH')==21)
+if ($.isNode()&&$.time('HH')>=21)
 notify.sendNotify(jsname,tz)
 	
 if (notifyInterval==1)
