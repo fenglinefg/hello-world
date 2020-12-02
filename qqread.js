a8eff2799ecdbca3d1ea6c032bbf2d0ebb89f081
ziye12 <59244461+ziye12@users.noreply.github.com>
2020-12-02 14:50:53 +0800

    企鹅读书  修复企鹅更新带来的打卡问题

--- qqread.js
+++ qqread.js
@@ -14,6 +14,7 @@
 11.28 修复错误
 12.1 调整通知为15次宝箱通知一次
 12.1 优化通知
+12.2 修复企鹅更新打卡不了的问题
 
 ⚠️cookie获取方法：
 
@@ -147,37 +148,37 @@ qqreadconfig();//时长查询
 else if (i==2)
 qqreadtask();//任务列表
 
-else if (i==3&&task.data.taskList[0].doneFlag==0)
+else if (i==3&&task.data.taskList[2].doneFlag==0)
 qqreadsign();//金币签到
 
 else if (i==4&&task.data.treasureBox.doneFlag==0)
 qqreadbox();//宝箱
 
-else if (i==5&&task.data.taskList[2].doneFlag==0)
+else if (i==5&&task.data.taskList[1].doneFlag==0)
 qqreadssr1();//阅读金币1
 
 else if (i==6&&config.data.pageParams.todayReadSeconds/3600<=maxtime)
 qqreadtime();//上传时长
 
-else if (i==7&&task.data.taskList[0].doneFlag==0)
+else if (i==7&&task.data.taskList[2].doneFlag==0)
 qqreadtake();//阅豆签到
 
-else if (i==8&&task.data.taskList[1].doneFlag==0)
+else if (i==8&&task.data.taskList[0].doneFlag==0)
 qqreaddayread();//阅读任务
 
-else if (i==9&&task.data.taskList[2].doneFlag==0)
+else if (i==9&&task.data.taskList[1].doneFlag==0)
 qqreadssr2();//阅读金币2
 
 else if (i==10&&task.data.taskList[3].doneFlag==0)
 qqreadvideo();//视频任务
 
-else if(i==11&&task.data.taskList[0].doneFlag==0)
+else if(i==11&&task.data.taskList[2].doneFlag==0)
 qqreadsign2();//签到翻倍
 
 else if (i==12&&task.data.treasureBox.videoDoneFlag==0)
 qqreadbox2();//宝箱翻倍
 
-else if (i==13&&task.data.taskList[2].doneFlag==0)
+else if (i==13&&task.data.taskList[1].doneFlag==0)
 qqreadssr3();//阅读金币3
 
 else if (i==14)
