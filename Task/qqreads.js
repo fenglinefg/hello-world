b3b8ae853a41e2655b38fb4b7289761c186702a7
Akira <e.akimoto.akira@gmail.com>
2020-12-02 11:37:42 +0800

    Update qqreads.js

--- Task/qqreads.js
+++ Task/qqreads.js
@@ -79,7 +79,7 @@ let qqreadhdArr = [], qqreadheaderVal = '',
   if (process.env.COOKIES_SPLIT){
       COOKIES_SPLIT = process.env.COOKIES_SPLIT;
   };
-  console.log(`============ 分隔符2${COOKIES_SPLIT} =============\n`);
+  console.log(`============ cookies分隔符为：${COOKIES_SPLIT} =============\n`);
   if (process.env.QQREAD_HEADER && process.env.QQREAD_HEADER.indexOf(COOKIES_SPLIT) > -1) {
   qqreadHD = process.env.QQREAD_HEADER.split(COOKIES_SPLIT);
   } else {
