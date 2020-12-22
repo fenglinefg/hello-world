025ad57a2b32bf20da059c66d5db90494a763ca7
demo2099 <available3099@gmail.com>
2020-12-22 10:33:47 +0800

    Update shareCode-auto.js

--- shareCode-auto.js
+++ shareCode-auto.js
@@ -8,11 +8,11 @@
 const $ = new Env("互助码");	
 const shareCodes = [	
   {	
-    zd: $.getdata("zd_shareCode1") || "qu73szyyke7tv4sgpjojpyiq6u",	
-    nc: $.getdata("nc_shareCode1") || "a1bc1602151a42d68fb8cf0ee93cc43d",	
-    mc: $.getdata("mc_shareCode1") || "MTAxODc2NTEzNDAwMDAwMDAyNzkyNjM4Mw==",	
-    ddgc: $.getdata("dd_shareCode1") || "P04z54XCjVWnYaS5kZ7fCKtjCEX",	
-    jxgc: $.getdata("jx_shareCode1") || "k3XRgh9SqTEODDhQVrfL1A==",	
+    zd: $.getdata("zd_shareCode1") ,	
+    nc: $.getdata("nc_shareCode1") ,	
+    mc: $.getdata("mc_shareCode1"),	
+    ddgc: $.getdata("dd_shareCode1"),	
+    jxgc: $.getdata("jx_shareCode1"),	
   },	
   {	
     zd: $.getdata("zd_shareCode2") || "",	
