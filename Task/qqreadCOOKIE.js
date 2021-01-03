e1b71a624c4d46695e0d8d65efd3d47e363608c6
ziye12 <59244461+ziye12@users.noreply.github.com>
2021-01-03 18:25:01 +0800

    独立cookie文件

---
+++ Task/qqreadCOOKIE.js
@@ -0,0 +1,19 @@
+//⚠️ck在``里面填写ck，多账号换行
+let qqreadbodyVal = ``;
+
+let qqreadtimeurlVal = ``;
+
+let qqreadtimeheaderVal = ``;
+
+
+
+let qqreadcookie = {
+        qqreadbodyVal: qqreadbodyVal,
+        qqreadtimeurlVal: qqreadtimeurlVal,
+        qqreadtimeheaderVal: qqreadtimeheaderVal
+}
+
+module.exports =  qqreadcookie
+  
+
+
