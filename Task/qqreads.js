330d49191bcf0251b7ed81fabb31955022375375
ziye12 <59244461+ziye12@users.noreply.github.com>
2021-01-04 15:14:48 +0800

    Update qqreads.js

--- Task/qqreads.js
+++ Task/qqreads.js
@@ -195,7 +195,13 @@ if ($.isNode()) {
 }
 
 // 今日0点时间戳
-const daytime = new Date(nowTimes.toLocaleDateString()).getTime();
+if ($.isNode()) {
+  daytime =
+    new Date(new Date().toLocaleDateString()).getTime() - 8 * 60 * 60 * 1000;
+} else {
+  daytime = new Date(new Date().toLocaleDateString()).getTime();
+}
+
 
 function GetCookie() {
   if ($request && $request.url.indexOf("addReadTimeWithBid?") >= 0) {
