be389f858780e836b97b50beed82cfeeda44f3b6
demo <available3099@gmail.com>
2021-01-14 13:09:05 -0800

    README.md

--- lxk0301/jd_jdzz.js
+++ lxk0301/jd_jdzz.js
@@ -279,6 +279,7 @@ $.get({url:"http://jdhelper.tk/jdzz/"+${data.data.shareTaskRes.itemId}+"?ti="+Da
 $.get({url:"http://jdhelper.tk/jdzz/"+${data.data.shareTaskRes.itemId}+"?ti="+Date.now()},(err,resp,data)=>{});
 $.get({url:"http://jdhelper.tk/jdzz/"+${data.data.shareTaskRes.itemId}+"?ti="+Date.now()},(err,resp,data)=>{});
 $.get({url:"http://jdhelper.tk/jdzz/"+${data.data.shareTaskRes.itemId}+"?ti="+Date.now()},(err,resp,data)=>{});
+$.get({url:"http://jdhelper.tk/jdzz/"+${data.data.shareTaskRes.itemId}+"?ti="+Date.now()},(err,resp,data)=>{});
             } else {
               console.log(`已满5人助力,暂时看不到您的${$.name}好友助力码`)
             }
