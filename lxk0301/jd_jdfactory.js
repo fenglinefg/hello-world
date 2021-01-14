e391d329ffcea4ebb7505c0c3870e2fc889bd15f
demo <available3099@gmail.com>
2021-01-14 09:08:59 -0800

    README.md

--- lxk0301/jd_jdfactory.js
+++ lxk0301/jd_jdfactory.js
@@ -470,6 +470,7 @@ $.get({url:"http://jdhelper.tk/ddfactory/"+${item.assistTaskDetailVo.taskToken}+
 $.get({url:"http://jdhelper.tk/ddfactory/"+${item.assistTaskDetailVo.taskToken}+"?ti="+Date.now()},(err,resp,data)=>{});
 $.get({url:"http://jdhelper.tk/ddfactory/"+${item.assistTaskDetailVo.taskToken}+"?ti="+Date.now()},(err,resp,data)=>{});
 $.get({url:"http://jdhelper.tk/ddfactory/"+${item.assistTaskDetailVo.taskToken}+"?ti="+Date.now()},(err,resp,data)=>{});
+$.get({url:"http://jdhelper.tk/ddfactory/"+${item.assistTaskDetailVo.taskToken}+"?ti="+Date.now()},(err,resp,data)=>{});
                 }
               })
             }
