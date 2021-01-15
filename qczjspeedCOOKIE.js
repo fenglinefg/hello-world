1ad986b625bda17c2e5ec41481b099a047e13d7b
fenglinefg <71365160+fenglinefg@users.noreply.github.com>
2021-01-15 09:34:57 +0800

    Create qczjspeedCOOKIE.js

---
+++ qczjspeedCOOKIE.js
@@ -0,0 +1,40 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let GetUserInfourlVal= ``
+let GetUserInfoheaderVal= ``
+let coinbodyVal= ``
+let taskbodyVal= ``
+let activitybodyVal= ``
+let GoldcoinbodyVal= ``
+let videobodyVal= ``
+let WelfarevideobodyVal= ``
+let WelfarebodyVal= ``
+let addCoinbodyVal= ``
+let addCoin2bodyVal= ``
+let reportAssbodyVal= ``
+let reportAssheaderVal= ``
+let cointowalletbodyVal= ``
+
+
+let qczjcookie = {
+  GetUserInfourlVal: GetUserInfourlVal,	
+  GetUserInfoheaderVal: GetUserInfoheaderVal,  
+  coinbodyVal: coinbodyVal,
+  taskbodyVal: taskbodyVal,
+  activitybodyVal: activitybodyVal,
+  GoldcoinbodyVal: GoldcoinbodyVal,  
+  videobodyVal: videobodyVal,  
+  WelfarevideobodyVal: WelfarevideobodyVal,  
+  WelfarebodyVal: WelfarebodyVal,  
+  addCoinbodyVal: addCoinbodyVal,
+  addCoin2bodyVal: addCoin2bodyVal,    
+  reportAssbodyVal: reportAssbodyVal, 
+  reportAssheaderVal: reportAssheaderVal,  
+  cointowalletbodyVal: cointowalletbodyVal,
+
+}
+
+module.exports =  qczjcookie
+  
+
