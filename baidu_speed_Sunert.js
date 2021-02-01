2d11687f2d81f4cad4b7de76fbbf030d1093d864
fenglinefg <71365160+fenglinefg@users.noreply.github.com>
2021-02-01 15:28:17 +0800

    Update baidu_speed_Sunert.js

--- baidu_speed_Sunert.js
+++ baidu_speed_Sunert.js
@@ -142,7 +142,7 @@ function userInfo() {
             
             username = "null";
             json = data.match(/window\.PAGE_DATA = (.+)/)[1];
-            json = formatJson(json);
+            json = json.substring(0, json.length-1);
             console.log("********************************************before********************************************************\n")
             console.log("\n")
             console.log("\n")
