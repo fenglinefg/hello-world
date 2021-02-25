a48c7bfb17f8bc183291065e662956f1bdcd4e02
demo <available3099@gmail.com>
2021-02-24 22:08:49 -0800

    klcw-app.js

--- Sunert/Task/Youth_Read.js
+++ Sunert/Task/Youth_Read.js
@@ -1,5 +1,5 @@
 /*
-更新时间: 2021-02-24 16:20
+更新时间: 2021-02-25 13:20
 Github Actions使用方法见[@lxk0301](https://raw.githubusercontent.com/lxk0301/scripts/master/githubAction.md) 使用方法大同小异
 
 请自行抓包，阅读文章和看视频，倒计时转一圈显示青豆到账即可，多看几篇文章和视频，获得更多包数据，抓包地址为"https://ios.baertt.com/v5/article/complete.json"，在Github Actions中的Secrets新建name为'YOUTH_READ'的一个值，拷贝抓包的请求体到下面Value的文本框中，添加的请求体越多，获得青豆次数越多，本脚本不包含任何推送通知
@@ -62,6 +62,9 @@ let indexLast = $.getdata('zqbody_index');
  $.begin = indexLast ? parseInt(indexLast,10) : 1;
  $.index = 0;
  $.log( "上次运行到第"+$.begin+"次终止，本次从"+(parseInt($.begin)+1)+"次开始");
+   if(smallzq){
+    $.log("\n 请注意缩减请求开关已打开‼️\n 如不需要，请强制停止，然后关闭Boxjs缩减请求开关‼️\n")
+  };
   for ( var i = indexLast ? indexLast:0; i < ReadArr.length; i++) {
     if (ReadArr[i]) {
       articlebody = ReadArr[i];
