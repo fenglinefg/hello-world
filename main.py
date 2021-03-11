047ebd4de28eeef8039bb3eab20bc15913a9b4b8
srcrs <srcrs@foxmail.com>
2021-03-11 10:03:02 +0800

    统一时区

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 import requests,json,time,re,login,logging,traceback,os,random,notify,datetime
 from lxml.html import fromstring
+import pytz
 
 #用户登录全局变量
 client = None
@@ -397,8 +398,18 @@ def getStorageFlow(username):
         datas.append(data)
     return datas
 
+#获取Asia/Shanghai时区时间戳
+def getTimezone():
+    timezone = pytz.timezone('Asia/Shanghai')
+    dt = datetime.datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S")
+    timeArray = time.strptime(dt, "%Y-%m-%d %H:%M:%S")
+    timeStamp = int(time.mktime(timeArray))
+    return timeStamp
+
 #获得流量包的还剩多长时间结束，返回形式时间戳
 def getflowEndTime(username):
+    #获得中国时间戳
+    now = getTimezone()
     #获得我的礼包页面对象
     html = getQuerywinning(username)
     #获得流量包到期的时间戳
@@ -417,7 +428,7 @@ def getflowEndTime(username):
             timeArray = time.strptime(end, "%Y-%m-%d %H:%M:%S")
             #得到时间戳
             timeStamp = int(time.mktime(timeArray))
-            endStamp.append(timeStamp-int(time.time()))
+            endStamp.append(timeStamp-now)
         else:
             #将找不到结束时间的流量包设置为不激活
             endStamp.append(86401)
