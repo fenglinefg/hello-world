9fff640e6cabfa8efed096cf72bc8109f27830db
q8qsTAUA <3441458892@qq.com>
2021-03-15 19:54:53 +0800

    update Task/shuqi.js.

--- Task/shuqi.js
+++ Task/shuqi.js
@@ -924,7 +924,7 @@ function GetCookie() {
                     } else {
                         $.setdata(shuqisyurlVal, "shuqisyurl" + $.idx);
                         $.log(
-                            `[${$.name + $.idx}] 获取收益shuqisyurlVal✅: 成功,shuqisyurlVal: $shuqisyurlVal}`
+                            `[${$.name + $.idx}] 获取收益shuqisyurlVal✅: 成功,shuqisyurlVal: ${shuqisyurlVal}`
                         );
                         $.msg($.name + $.idx, `获取收益shuqisyurlVal: 成功🎉`, ``);
                         $.setdata(shuqisybodyVal, "shuqisybody" + $.idx);
